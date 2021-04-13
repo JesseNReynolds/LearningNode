5f6a66ccfb9c5ce35337de3cb1a6794a5282418b
JesseNReynolds <jessenreynolds@gmail.com>
2021-04-13 15:46:08 -0600

    First modules extra

--- tutorial/03-modules/app.js
+++ tutorial/03-modules/app.js
@@ -1,6 +1,9 @@
 const names = require('./names')
 const sayHi = require('./utils')
+const data = require('./another-way')
+require('./addValues.js')
 
+console.log(data)
 sayHi('Susan')
 sayHi(names.john)
 sayHi(names.peter)
